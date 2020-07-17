05c1f89cd44bf35bd05701de0f901c4ccad4580c
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-17 15:23:08 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by KPS on 7/17/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    double d_num = 5.44444;
+  if ((d_num % 1) == 0) {
+        System.out.println("It's not a double number");
+    } else {
+        System.out.println("It's a double number");
+    }
+}
+}
